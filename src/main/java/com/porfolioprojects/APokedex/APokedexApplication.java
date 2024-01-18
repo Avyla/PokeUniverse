5ed39578b9f3e6502fc6a06174488a21f8fef33a
Avyla <eliamavila780@gmail.com>
2024-01-18 18:27:34 -0500

    first commit

---
+++ src/main/java/com/porfolioprojects/APokedex/APokedexApplication.java
@@ -0,0 +1,13 @@
+package com.porfolioprojects.APokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APokedexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APokedexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/porfolioprojects/APokedex/APokedexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfolioprojects.APokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class APokedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
